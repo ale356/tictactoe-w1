1ff31c743c1e89b6cd90c6f246f01db2d275c80e
Alejandro <al225vh@student.lnu.se>
2022-09-07 10:30:06 +0200

    Add function printBoard

--- app.js
+++ app.js
@@ -5,6 +5,26 @@ const reader = readline.createInterface({
   output: process.stdout
 })
 
+
+console.log("Welcome to tic tac toe game.")
+console.log("You have been assigned to play as X")
+
+// Print the game board.
+
+
+
 const positionQuestion = reader.question('Position: ')
 
-// 
+// A function that prints out the board in the console.
+
+function printBoard (position) {
+  const square = "| |"
+  const amountOfSquares = 9;
+
+  // Print each square.
+  for(let i = 0; i < amountOfSquares; i++) {
+	
+  }
+
+	
+}
